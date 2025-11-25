e1951a33705f3cfecbada527f4b34faf192f41e8
Pamela Timi <w22012025@gmail.com>
2025-11-25 11:01:39 +0100

    Co-authored-by: LESPINASSE MARINI <LaurieLM@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
-export default function Header() {
+ export default function Header() {
   return (
-    <>
-      <h1>Start</h1>
-    </>
-  )
+    <header className="flex items-center justify-between p-4 bg-black text-white">
+      {/* Logo */}
+      <div className="text-2xl font-bold"> Cinéma Paris</div>
+
+      {/* Menu */}
+      <nav className="flex gap-6">
+        <a href="/" className="hover:text-yellow-300">Home</a>
+        <a href="/dashboard" className="hover:text-yellow-300">Dashboard</a>
+        <a href="/graphs" className="hover:text-yellow-300">Graphiques</a>
+      </nav>
+
+      {/* Icone */}
+      <button className="bg-yellow-400 text-black px-3 py-1 rounded-lg">
+        Login
+      </button>
+    </header>
+  );
 }
+    
+  
