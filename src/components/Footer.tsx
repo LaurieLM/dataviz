760c6fdf8684d1a92099fb7984507b9063e1edf3
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-25 10:35:06 +0100

    feature / component Footer + install TailWind

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() { 
+    return (
+        <footer>
+            <a href="">A propos</a>
+            <p>© 2025 Dataviz App. All rights reserved.</p>
+        </footer>
+    )
+}
