d61ef28b6fa645c153c5d9f414da779315912d10
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-25 14:54:17 +0100

    fix / rename

--- src/components/TeamPresentation.tsx
+++ src/components/TeamPresentation.tsx
@@ -1,12 +1,12 @@
 export default function TeamPresentation() {
 return (
-<section className="flex flex-col items-center">
+<section className="flex flex-col items-center flex-[1] justify-evenly">
     <h1 className="text-[4vw] font-bold">L'Equipe</h1>
     <p className="w-[54vw] text-center font-[100]">
         Notre équipe est composée de deux membres passionnés par le développement web. Chacun apporte ses compétences
         uniques pour faire de ce projet un succès. <br /><br />
-        - Laurie : Développeuse front-end, spécialisée en React et design UI/UX.<br />
-        - Pamela : Développeur back-end, expert en API et gestion de bases de données.<br /><br />
+        - <strong>Laurie</strong> <br />
+        - <strong>Pamela</strong> <br /><br />
         Ensemble, nous avons travaillé en solo-programming ainsi qu'en pair-programming pour les tâches les plus
         difficiles.
     </p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import App from './pages/Home.js'
+import AppHome from './pages/Home.js'
+import AppAbout from './pages/About.js'
+import AppGraphics from './pages/Graphics.js'
 
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <AppGraphics />
 )
