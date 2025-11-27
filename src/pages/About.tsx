8f7da369d615522fbebbe3f997d00d553d0fb28e
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-27 12:09:04 +0100

    fix/refactoring graphics + data

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 import TeamPresentation from '../components/TeamPresentation'
 
-export default function AppAbout() {
+export default function About() {
 
   return (
     <div className='flex flex-col justify-between w-[100vw] h-[100vh]'>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import ProjectPresentation from '../components/ProjectPresentation'
 
-export default function AppHome() {
+export default function Home() {
 
   return (
     <div className='flex flex-col justify-between w-[100vw] h-[100vh]'>
