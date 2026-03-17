90ad3157f48e775e6a36735d3ad64137e692545d
w2201 <w22012025@gmail.com>
2026-03-17 01:19:32 +0100

    chore: deploy gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,29 +7,31 @@ import About from "./pages/About";
 import Graphics from "./pages/Graphics";
 import ErrorPage from "./pages/ErrorPage";
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new QueryClient();
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            { index: true, element: <Home /> },
-            { path: "about", element: <About /> },
-            { path: "graphs", element: <Graphics /> },
-        ],
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "graphs", element: <Graphics /> },
+      ],
     },
-]);
-
+  ],
+  { basename: "/dataviz" },
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={client}>
-            <RouterProvider router={router} />
-            <ReactQueryDevtools />
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
